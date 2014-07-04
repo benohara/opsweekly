8b063d9fd93e723cb485605f4be0171b14ac3f2d
Ben O'Hara <bohara@gmail.com>
2014-07-04 19:26:50 +1000

    pass previous id back from forms

--- add_meeting_notes.php
+++ add_meeting_notes.php
@@ -7,11 +7,16 @@ if (!connectToDB()) {
 } else {
     $timestamp = time();
     $username = getUsername();
+    $id = mysql_real_escape_string($_POST['id']);
     $range_start = mysql_real_escape_string($_POST['range_start']);
     $range_end = mysql_real_escape_string($_POST['range_end']);
     $report_id = generateMeetingNotesID($range_start, $range_end);
     $notes = mysql_real_escape_string($_POST['weeklynotes']);
-    $query = "INSERT INTO meeting_notes (report_id, range_start, range_end, timestamp, user, notes) VALUES ('$report_id', '$range_start', '$range_end', '$timestamp', '$username', '$notes')";
+    if ($id == null ) {
+        $query = "INSERT INTO meeting_notes (report_id, range_start, range_end, timestamp, user, notes) VALUES ('$report_id', '$range_start', '$range_end', '$timestamp', '$username', '$notes')";
+    } else {
+        $query = "UPDATE meeting_notes (report_id, range_start, range_end, timestamp, user, notes) VALUES ('$report_id', '$range_start', '$range_end', '$timestamp', '$username', '$notes')";
+    }
     if (!mysql_query($query)) {
         echo "Database update failed, error: " . mysql_error();
     } else {
